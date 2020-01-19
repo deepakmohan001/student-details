b5c68dd0e72a3c67a88b22880bbce0a7e10278d3
deepakmohan001 <deepakdec10@gmail.com>
2020-01-19 15:05:21 +0530

    student details

---
+++ edit.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Student Entry</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="search.php">Search</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="edit.php">Edit</a>
+            </li>
+          </ul>
+    </nav>
+    <h2>
+        Edit
+    </h2>
+    <form method="GET">
+    <table class="table">
+        <tr>
+            <td>
+                student address
+            </td>
+            <td>
+                <input type="text" class="form-control"name="getAddress">
+            </td>
+        </tr>
+        <tr>
+            <td>
+
+            </td>
+            <td>
+                <button type="submit" class="btn btn-danger"name="submit">
+                    Edit
+                </button>
+            </td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
+<?php
+if(isset($_GET["submit"]))
+{
+$Address=$_GET["getAddress"];
+echo $Address;
+}
+?>
---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Student Entry</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="search.php">Search</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="edit.php">Edit</a>
+            </li>
+          </ul>
+    </nav>
+<form method="GET">
+<table class="table">
+<tr>
+    <td>
+        name
+    </td>
+    <td>
+        <input type="text" name="getName"class="form-control">
+    </td>
+</tr>
+<tr>
+    <td>
+        roll number
+    </td>
+    <td>
+        <input type="text" name="getRoll" class="form-control">
+    </td>
+</tr>
+<tr>
+    <td>
+        college
+    </td>
+    <td>
+        <input type="text" name="getCollege" class="form-control">
+    </td>
+</tr>
+<tr>
+    <td>
+        Admission number
+    </td>
+    <td>
+        <input type="text" name="getAddmno" class="form-control">
+    </td>
+</tr>
+<tr>
+    <td>
+    
+    </td>
+    <td>
+        <button type="submit" name="submit" class="btn btn-danger">
+            submit
+        </button>
+    </td>
+</tr>
+</table>
+</form>
+</body>
+</html>
+<?php
+if(isset($_GET["submit"]))
+{
+    $Name=$_GET["getName"];
+    $Roll=$_GET["getRoll"];
+    $College=$_GET["getCollege"];
+    $Addmn=$_GET["getAddmno"];
+    echo $Name;
+    echo "<br>";
+    echo $Roll;
+    echo "<br>";
+    echo $College;
+    echo "<br>";
+    echo $Addmn;
+}
+?>
